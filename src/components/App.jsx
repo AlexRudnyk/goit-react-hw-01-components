204a879d292596d531a5b6becbcca056898a4335
AlexRudnyk <alexander.rudnyk@ukr.net>
2022-10-22 23:41:03 +0300

    Makes task 03 friends

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,10 @@
 import { Container } from './container/Container';
 import { Profile } from './profile/Profile';
 import { Statistics } from './statistics/Statistics';
+import { FriendList } from './friendList/FriendList';
 import user from 'data/user.json';
 import data from 'data/data.json';
+import friends from 'data/friends.json';
 
 export const App = () => {
   return (
@@ -17,6 +19,7 @@ export const App = () => {
         likes={user.stats.likes}
       />
       <Statistics title={'Upload stats'} stats={data} />
+      <FriendList friends={friends} />
     </Container>
   );
 };
