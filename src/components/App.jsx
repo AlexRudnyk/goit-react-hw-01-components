bcecdf362e8a7ee56081575a86490e0f21a4c1dc
AlexRudnyk <alexander.rudnyk@ukr.net>
2022-10-23 00:38:13 +0300

    Make task 04 transaction

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,9 +2,11 @@ import { Container } from './container/Container';
 import { Profile } from './profile/Profile';
 import { Statistics } from './statistics/Statistics';
 import { FriendList } from './friendList/FriendList';
+import { TransactionHistory } from './transactionHistory/TransactionHistory';
 import user from 'data/user.json';
 import data from 'data/data.json';
 import friends from 'data/friends.json';
+import transactions from 'data/transactions.json';
 
 export const App = () => {
   return (
@@ -20,6 +22,7 @@ export const App = () => {
       />
       <Statistics title={'Upload stats'} stats={data} />
       <FriendList friends={friends} />
+      <TransactionHistory transactions={transactions} />
     </Container>
   );
 };
