61dff7eada7f49e27f46dc4f175a5b668f06257d
AlexRudnyk <alexander.rudnyk@ukr.net>
2022-10-23 13:05:20 +0300

    Creates add component FriendListItem

--- src/components/friendList/FriendList.jsx
+++ src/components/friendList/FriendList.jsx
@@ -1,34 +1,22 @@
 import PropTypes from 'prop-types';
 import css from './FriendList.module.css';
+import { FriendListItem } from 'components/friendListItem/FriendListItem';
 
 export const FriendList = ({ friends }) => {
   return (
     <ul className={css.friendList}>
       {friends.map(friend => (
-        <li className={css.item} key={friend.id}>
-          <span
-            className={friend.isOnline ? css.isOnline : css.isOffline}
-          ></span>
-          <img
-            className={css.avatar}
-            src={friend.avatar}
-            alt={friend.name}
-            width="48"
-          />
-          <p className={css.name}>{friend.name}</p>
-        </li>
+        <FriendListItem
+          avatar={friend.avatar}
+          name={friend.name}
+          isOnline={friend.isOnline}
+          key={friend.id}
+        />
       ))}
     </ul>
   );
 };
 
 FriendList.propTypes = {
-  friends: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      name: PropTypes.string.isRequired,
-      avatar: PropTypes.string.isRequired,
-      isOnline: PropTypes.bool,
-    })
-  ),
+  friends: PropTypes.arrayOf(PropTypes.shape),
 };
